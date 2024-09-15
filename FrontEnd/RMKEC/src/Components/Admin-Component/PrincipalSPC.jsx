b689687decc68f9dc4b7be1d75e63a82b68cb121
likesh50 <like22050.it@rmkec.ac.in>
2024-09-15 20:36:29 +0530

    ai report ui updated

--- FrontEnd/RMKEC/src/Components/Admin-Component/PrincipalSPC.jsx
+++ FrontEnd/RMKEC/src/Components/Admin-Component/PrincipalSPC.jsx
@@ -78,7 +78,7 @@ const CustomTooltip = ({ active, payload }) => {
 
 const PrincipalSPC = ({ data }) => (
   <ResponsiveContainer >
-    {console.log(data)};
+    
     <div style={{
       display: 'flex',
       justifyContent: 'center',
