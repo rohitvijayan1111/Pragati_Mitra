9af526ac84106126b0d9826d816a447916fa2411
rohit-vijayan <broh22012.it@rmkec.ac.in>
2024-09-18 10:32:45 +0530

    my changes

--- FrontEnd/RMKEC/vite.config.js
+++ FrontEnd/RMKEC/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Pragati_Mitra/"
+  base:"/Pragati_Mitra/",
+  build: {
+    outDir: 'build', // This specifies the output directory
+  },
 })
+
